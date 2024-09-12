1747b47e452a39f7a8d3d10f378cea8fec7a722b
Hosseyn Alavi Arjasi <hosseyn.alavi@Hosseyn--MacBook-Pro.local>
2024-09-12 17:18:15 +0200

    f

--- index.ts
+++ index.ts
@@ -4,7 +4,7 @@ const app = express();
 const PORT = 4000;
 
 app.get("/home", (req, res) => {
-    res.status(200).json("Welcome, your app is working well");
+    res.status(200).json("Welcome, your. app is working well");
 });
 
 app.listen(PORT, () => {
